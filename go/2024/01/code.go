be64509dfc31c2f59bf7b1aa1098d11b0eec6696
reptilianeye <piotr.rzadkowski@swmansion.com>
2024-12-01 06:53:06 +0100

    day 1 solved

--- go/2024/01/code.go
+++ go/2024/01/code.go
@@ -1,11 +1,60 @@
 package main
 
 import (
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
+	"github.com/thoas/go-funk"
 )
 
 func main() {
 	aoc.Harness(run)
+}
+
+func parseInput(input string) [][]int {
+	in := [][]int{}
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		els := strings.Split(line, "   ")
+		elsInt := funk.Map(els, func(el string) int {
+			val, _ := strconv.Atoi(el)
+			return val
+		}).([]int)
+		in = append(in, elsInt)
+	}
+	return in
+}
+
+func solvePart1(input [][]int) int {
+	left := []int{}
+	right := []int{}
+	for _, pair := range input {
+		left = append(left, pair[0])
+		right = append(right, pair[1])
+	}
+	slices.Sort(left)
+	slices.Sort(right)
+	diff := 0
+	for i := range left {
+		diff += int(math.Abs(float64(left[i] - right[i])))
+	}
+	return diff
+}
+func solvePart2(input [][]int) int {
+	left := []int{}
+	counts := make(map[int]int)
+	for _, pair := range input {
+		left = append(left, pair[0])
+		counts[pair[1]]++
+	}
+	similarity := 0
+	for _, l := range left {
+		similarity += l * counts[l]
+	}
+	return similarity
 }
 
 // on code change, run will be executed 4 times:
@@ -16,9 +65,10 @@ func main() {
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
 	// when you're ready to do part 2, remove this "not implemented" block
+	parsedInput := parseInput(input)
 	if part2 {
-		return "not implemented"
+		return solvePart2(parsedInput)
 	}
 	// solve part 1 here
-	return 42
+	return solvePart1(parsedInput)
 }
